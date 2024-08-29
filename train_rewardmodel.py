8a39a97efa06136db6072844583bd13e457b6561
linlizhai <linlizhai@trip.com>
2024-08-29 17:01:49 +0800

    setup

--- train_rewardmodel.py
+++ train_rewardmodel.py
@@ -48,6 +48,7 @@ def train_rewardmodel(config):
     traindata = dataprocess.load_data(config.traindata_path)
 
     device = torch.device("cuda" if torch.cuda.is_available() and config.use_cuda else "cpu")
+    print(f"config.model_path: {config.model_path}")
     model = AutoModel.from_pretrained(config.model_path)
     tokenizer = AutoTokenizer.from_pretrained(config.model_path, use_fast=True, padding_side="right")
     model.to(device)
@@ -106,6 +107,7 @@ if __name__ == '__main__':
     # os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
     parsed_arguments = parser.parse_args()
     config = RewardModel_Config.init_from_parsed_args(parsed_arguments)
+    print(f"config: {config.to_json_string()}")
     train_rewardmodel(config)
     # traindata = dataprocess.load_data(config.traindata_path)
     # 
