4a6a6c04c93ecb8cb5111bed900a280f5d6f5a6f
linlizhai <linlizhai@trip.com>
2024-09-02 15:21:55 +0800

    setup

--- run_model.sh
+++ run_model.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=2 train_rewardmodel.py
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+torchrun --nproc_per_node=2 train_rewardmodel.py
